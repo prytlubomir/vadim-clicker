c57518ead0cbee355cfaa74c202d2eb1aeefb2b5
Liubomyr Pryt <prytlubomyr@gmail.com>
2025-11-13 20:36:59 +0000

    Removed pynput, added keyboard, deleted pynput dependent code.

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
-import time
-
-import pynput.mouse as mouse
-import pynput.keyboard as keys
-
-
-activated = False
-
-
-def on_release(key):
-    global activated
-    activated = not activated
-    sk = str(key).strip()
-    if isinstance(key, keys.Key):
-        print('key', key.name)
-    elif isinstance(key, keys.KeyCode):
-        print('keycode', key.char)
-
-def clicker(period: int = 1):
-    controller = mouse.Controller()
-    while True:
-        if activated:
-            time.sleep(period)
-            controller.click(mouse.Button.left)
-
 
 def main():
-    listener = keys.Listener(on_release=on_release)
-    listener.start()
-    while True:
-        pass
+    print('hello world')
 
 
 if __name__ == "__main__":
