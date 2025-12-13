a4661013717f6c531651714292e04edf36f25d6f
Liubomyr Pryt <prytlubomyr@gmail.com>
2025-12-13 00:20:18 +0000

    URGENT UPDATE: Added Left Mouse Button holding function, reworker structure to allow new functionality. TODO: REFACTOR

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import time
 from threading import Thread
 
+from typing import Callable
+
 import mouse
 import keyboard
 
@@ -29,40 +31,65 @@ def clicker(timeout: int = 1):
         mouse.click()
 
 
-def add_hotkey(hotkey: str):
+def holder():
+    ''' Holds the left mouse button untill called again '''
+    global holder_active
+    if holder_active:
+        mouse.release()
+        holder_active = False
+    mouse.press()
+    holder_active = True
+
+
+def add_hotkey(hotkey: str, func: Callable):
     ''' Add a trigger button '''
-    handler = keyboard.add_hotkey(hotkey=hotkey, callback=toggle)
+    handler = keyboard.add_hotkey(hotkey=hotkey, callback=func)
     return handler
 
 
-def remap_hotkey(new_hotkey: str = ''):
+def remap_hotkey(function_id: str):
     ''' Change the trigger button '''
-    global hotkey_handler
+    global trigger_map
+    hotkey_handler = trigger_map[function_id][0]
+    
     keyboard.remove_hotkey(hotkey_handler)
-    if new_hotkey:
-        hotkey_handler = add_hotkey(new_hotkey)
-        return hotkey_handler
+    
+    # if new_hotkey:
+    #     hotkey_handler = add_hotkey(new_hotkey, trigger_map[function_id][1])
+    #     trigger_map[function_id][0] = hotkey_handler
+    #     return hotkey_handler
+    
     nh = keyboard.read_hotkey()
-    hotkey_handler = add_hotkey(nh)
+    hotkey_handler = add_hotkey(nh, trigger_map[function_id][1])
+    trigger_map[function_id][0] = hotkey_handler
+
     return nh
-        
 
 
 def main():
     ''' The app '''
-    global hotkey_handler
-    hotkey_handler = add_hotkey('f7')
+    clicking_trigger = add_hotkey('f7', trigger_map['0'][1])
+    pressing_trigger = add_hotkey('f8', trigger_map['0'][1])
     
-    print('Your current Trigger is: f7')
+    with open("start_message.txt") as smf:
+        sm = smf.read()
+        print(sm)
+    
+    print('Changing the Holder trigger is currently not supported.')
     while True:
         input("Press Enter to change the trigger...")
         time.sleep(0.1)
-        new_trigger = remap_hotkey()
+        new_trigger = remap_hotkey('0')
         print(f"Your new Trigger is: {new_trigger}")
     #keyboard.wait()
 
 
 if __name__ == "__main__":
+    trigger_map = {
+        '0' : [None, toggle],
+        '1' : [None, holder]
+    }
+    holder_active = False
     active = False
     thread = Thread()
     main()
