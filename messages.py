4c77371e4b9a7b63d4bb446906c72017bfa5f53f
Liubomyr Pryt <prytlubomyr@gmail.com>
2025-12-16 03:26:45 +0000

    Moving from pyinstaller to nuitka; Moved ASCII to the src directory; Changet ASCII.

--- messages.py
+++ messages.py
@@ -1,9 +1,8 @@
-start_message = '''
-__      __       _ _           _        _____ _ _      _             
- \ \    / /      | (_)         ( )      / ____| (_)    | |            
-  \ \  / /_ _  __| |_ _ __ ___ |/ ___  | |    | |_  ___| | _____ _ __ 
-   \ \/ / _` |/ _` | | '_ ` _ \  / __| | |    | | |/ __| |/ / _ \ '__|
-    \  / (_| | (_| | | | | | | | \__ \ | |____| | | (__|   <  __/ |   
-     \/ \__,_|\__,_|_|_| |_| |_| |___/  \_____|_|_|\___|_|\_\___|_|   
-'''
-remapping_instruction = 'To change a trigger map, type in the ID of the functio you want to remap, and press the new trigger key.'
+import os
+# nuitka stores data files in a temporary folder, created on application startup, and removed after closing
+# relative paths are relative to the exe
+loc = os.path.join(os.path.dirname(__file__), 'src/ascii.txt')
+f = open(loc)
+start_message = f.read()
+f.close()
+remapping_instruction = "To change a trigger map, type in the ID of the functio you want to remap, and press the new trigger key."
