7cb405dd69367745fcbb95cece79d20fb62bf0e7
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-01-15 03:03:49 +0000

    Created NumberInput widget; Created Input class, as a prototype for all inputs, and moved TriggerInput's styling there; Disabled redundant logging; Rearanged imports with Ruff.

--- gui.py
+++ gui.py
@@ -1,35 +1,32 @@
 from enum import Enum
 
-from kivy.clock import Clock
 from kivy.config import Config
-from kivy.metrics import Metrics
-from kivy.uix.gridlayout import GridLayout
-from kivy.utils import rgba
-
-from kivy.app import App
-from kivy.properties import (
-    ColorProperty,
-    NumericProperty,
-    ObjectProperty,
-    StringProperty,
-    VariableListProperty,
-    BooleanProperty,
-)
-from kivy.uix.stacklayout import StackLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
-
-# from kivy.config import Config
-# from kivy.core import core_select_lib
-from triggers import Trigger
 
 Config.set("input", "mouse", "mouse,multitouch_on_demand")  # remove red dots
 Config.set("graphics", "width", "250")
 Config.set("graphics", "height", "229")
 Config.set("graphics", "resizable", False)
-
+from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.metrics import Metrics, dp
+from kivy.properties import (
+    BooleanProperty,
+    ColorProperty,
+    NumericProperty,
+    ObjectProperty,
+    StringProperty,
+    VariableListProperty,
+)
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.utils import rgba
+
+from triggers import Trigger
 
 
 class ButtonStatus(Enum):
@@ -88,10 +85,11 @@ class TopLayout(Section):
 
 
 
-class TriggerInput(Button):
-    current_trigger = StringProperty()
+class Input(Widget):
+    
+    value = StringProperty()
     status = ObjectProperty(ButtonStatus.idle)
-
+    
     bg_color = ColorProperty(rgba((1, 1, 1, 1)))
     bg_color_idle = ColorProperty(rgba((1, 1, 1, 1)))
     bg_color_hover = ColorProperty(rgba((1, 1, 1, 1)))
@@ -101,19 +99,17 @@ class TriggerInput(Button):
     border_color_idle = ColorProperty(rgba((1, 1, 1, 1)))
     border_color_hover = ColorProperty(rgba((1, 1, 1, 1)))
     border_color_active = ColorProperty(rgba((1, 1, 1, 1)))
-
+    
     border_width = NumericProperty(1)
-
+    
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if "text" in kwargs.keys():
-            self.current_trigger = kwargs["text"]
-        else:
-            self.current_trigger = "f7"
-            Window.bind(on_motion=self.on_motion)
-
+        Window.bind(on_motion=self.on_motion)
+    
+    
     def on_status(self, *args):
-        print("on_status", *args)
+        # print(f"--- on_status: {args} ---")
         match self.status:
             case ButtonStatus.idle:
                 self.bg_color = self.bg_color_idle
@@ -125,11 +121,14 @@ class TriggerInput(Button):
                 self.bg_color = self.bg_color_active
                 self.border_color = self.border_color_active
 
+
     def on_press(self):
+        print(f'--- {self} on_press ---')
         self.status = ButtonStatus.active
-        print(self.current_trigger)
+
 
     def on_release(self):
+        # print('--- on_release ---')
         if self.collide_point(*Window.mouse_pos):
             self.status = ButtonStatus.hover
         else:
@@ -148,7 +147,7 @@ class TriggerInput(Button):
     #     return False
 
     def on_motion(self, window, etype, me):
-        print(etype, self.status, me)
+        # print(etype, self.status, me)
         pos_x = me.spos[0] * window.width
         pos_y = me.spos[1] * window.height
         collision = self.collide_point(pos_x, pos_y)
@@ -159,6 +158,34 @@ class TriggerInput(Button):
             self.status = ButtonStatus.idle
         return False
         # return self._hover(window, etype, me)
+
+
+class TriggerInput(Input, Button):
+    current_trigger = StringProperty()
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if "text" in kwargs.keys():
+            self.current_trigger = kwargs["text"]
+        else:
+            self.current_trigger = "f7"
+    
+    
+    def on_current_trigger(self, *args):
+        self.text = self.current_trigger
+
+
+class NumberInput(Input, TextInput):
+    
+    multiline = BooleanProperty(False)
+    input_filter = ObjectProperty('float')
+    
+    def on_touch_down(self, touch):
+        hit = super().on_touch_down(touch)
+        if hit:
+            super().on_press()
+        return hit
 
 
 class Header(Label):
@@ -189,10 +216,10 @@ class MyLayout(StackLayout):
 class RootWidget(Widget):
     padding = NumericProperty(7)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        print(f"++++++++++++++++ {self.size} ++++++++++++++++")
-        Clock.schedule_interval(self._print_density, 1)
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     print(f"++++++++++++++++ {self.size} ++++++++++++++++")
+    #     #Clock.schedule_interval(self._print_density, 1)
 
     def _print_density(self, dt):
         print(f"dt={dt}, density={Metrics.density}, db={Metrics.dp}, sp={Metrics.sp}")
