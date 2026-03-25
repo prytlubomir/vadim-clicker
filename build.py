0c4db49538fc0d682ead7c8ae1fb2104c1c6fd53
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-03-25 03:22:55 +0000

    Fixed GUI build.

--- build.py
+++ build.py
@@ -2,7 +2,13 @@
 import os
 
 
-BUILD_CMD = "py -m nuitka main.py --mode=onefile --include-data-files=./src/ascii.txt=src/ascii.txt"
+# split into lines for readability
+BUILD_CMD = """py -m nuitka main.py --mode=standalone 
+               --include-data-dir=./src=src 
+               --include-data-dir=./uix=uix 
+               --include-data-files=./vadimsclicker.kv=vadimsclicker.kv
+"""
+BUILD_CMD = ' '.join(BUILD_CMD.split()) # joined into a sigle line for cross platform support
 
 
 def build():
