f10efc4f56d656e9ffb95e3bd08cf4bb2a9b4358
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-03-25 03:18:13 +0000

    Fixed GUI build.

--- build.py
+++ build.py
@@ -2,11 +2,38 @@
 import os
 
 
-BUILD_CMD = "py -m nuitka main.py --mode=onefile --include-data-files=./src/ascii.txt=src/ascii.txt"
+# split into lines for readability
+BUILD_CMD = """py -m nuitka main.py --mode=standalone 
+               --include-data-dir=./src=src 
+               --include-data-dir=./uix=uix 
+               --include-data-files=./vadimsclicker.kv=vadimsclicker.kv
+"""
+BUILD_CMD = ' '.join(BUILD_CMD.split()) # joined into a sigle line for cross platform support
+
+def scan_dir(path):
+    lst = os.listdir(path)
+    visible = list(filter(lambda x: x[0] is not '.', lst))
+    dirs = list(filter(lambda x: os.path.isdir(f'{path}/{x}'), visible))
+    files = list(filter(lambda x: os.path.isfile(f'{path}/{x}'), visible))
+    kvs = list(filter(lambda f: '.kv' in f, files))
+    
+    for dir in dirs:
+        dc = scan_dir(os.path.join(path, dir))
+        kvs += list(map(lambda f: dir+'/'+f, dc))
+
+    return kvs
+
+
+def generate_args(kvs):
+    gcmd = lambda p: f"--include-data-files=./{p}={p}"
+    cmds = [gcmd(f'uix/{kv}') for kv in kvs]
+    return cmds
 
 
 def build():
     ''' Execute the command for building the app '''
+    kvs = scan_dir('uix')
+    cmd = BUILD_CMD+' '+' '.join(generate_args(kvs))
     os.system(BUILD_CMD)
 
 
