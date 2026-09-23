bd3963ac7221f05ed156a77599c994e83f0c0724
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-09-23 23:10:57 +0100

    Cleanup

--- uix/numberinput/numberinput.py
+++ uix/numberinput/numberinput.py
@@ -13,26 +13,11 @@ class NumberInput(Input, TextInput):
     input_filter = ObjectProperty('float')
     trigger = ObjectProperty()
 
+    def _calc_timeout(self):
+        return 1 / float(self.text)
 
-    def __init__(self, *args, **kwargs):
-        print('NumberInput init')
-        super().__init__(*args, **kwargs)
-
-
-    def _calc_timeout(self, period):
-        return 1 / period
-
-    def on_enter(self, i, value, *args, **kwargs):
-        super().on_enter(i, value, *args, **kwargs)
-        print('entered:', value)
-
-    def on_text(self, i, value, *args, **kwargs):
-        # super().on_text(i, value, *args, **kwargs)
-        print('texted:', value)
-
-    def on_focus(self, i, v, *args, **kwargs):
-        super().on_focus(i, v, *args, **kwargs)
-        print('text input focus', v)
-        if not v:
-            print('Entered:', self.text)
-            self.trigger.set_timeout(self._calc_timeout(float(self.text)))
+    def on_focus(self, instance, value, *args, **kwargs):
+        super().on_focus(instance, value, *args, **kwargs)
+        if not value:
+            timeout = self._calc_timeout()
+            self.trigger.set_timeout(timeout)
