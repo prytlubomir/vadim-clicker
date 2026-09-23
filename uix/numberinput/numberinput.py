2f136b1b34a549510b649ac2861dedd7386623b4
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-09-23 22:58:51 +0100

    Clicking Period Settings: input handling done

--- uix/numberinput/numberinput.py
+++ uix/numberinput/numberinput.py
@@ -11,3 +11,21 @@ class NumberInput(Input, TextInput):
     
     multiline = BooleanProperty(False)
     input_filter = ObjectProperty('float')
+
+
+    def __init__(self, *args, **kwargs):
+        print('NumberInput init')
+        super().__init__(*args, **kwargs)
+
+
+    def on_enter(self, i, value, *args, **kwargs):
+        super().on_enter(i, value, *args, **kwargs)
+        print('entered:', value)
+
+    def on_text(self, i, value, *args, **kwargs):
+        # super().on_text(i, value, *args, **kwargs)
+        print('texted:', value)
+
+    def on_focus(self, i, v, *args, **kwargs):
+        super().on_focus(i, v, *args, **kwargs)
+        print('text input focus', v)
